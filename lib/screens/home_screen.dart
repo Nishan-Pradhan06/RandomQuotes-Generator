057ee73456b5bd7a8bfe2d054ce9c1febe86cf9a
nishan <nishanpradhan500@gmail.com>
2023-10-08 07:42:46 +0545

    ui-updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
 // import 'package:learngin/screens/second_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,38 +15,71 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String quotes = "😁Generate Random quotes😁";
   String author = "";
+  // int length =int.parse("");
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.deepPurple.shade100,
         appBar: AppBar(
-          title: const Text("Random Quotes"),
+          title: Text(
+            "Random Quotes",
+            style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 20)),
+          ),
         ),
         body: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 80, left: 20, right: 20),
-              child: Text(
-                quotes,
-                textAlign: TextAlign.start,
-                style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20, right: 30),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Text(
-                  "-$author",
-                  style: const TextStyle(fontWeight: FontWeight.w400),
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                // height: 150,
+                height: MediaQuery.of(context).size.height / 3,
+                // decoration: BoxDecoration(border: Border.all(width: 8)),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 25, left: 10, right: 10, bottom: 10),
+                      child: Text(
+                        quotes,
+                        textAlign: TextAlign.start,
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: .4,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, right: 30),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          "-$author",
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 100),
               child: Center(
-                child: ElevatedButton(
+                child: CupertinoButton.filled(
+                  // disabledColor: CupertinoColors.quaternarySystemFill,
                   onPressed: () async {
                     var url = Uri.parse('https://api.quotable.io/random');
                     var response = await http.get(url);
