59a5d53efe8de4fc146c198daca311e8bd23e9de
nishan <nishanpradhan500@gmail.com>
2023-10-20 11:50:24 +0545

    remove appbar icon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,15 +21,17 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future getQuotes() async {
     showDialog(
-        context: context,
-        builder: (context) {
-          return const Center(
-            child: CircularProgressIndicator(
-              valueColor:
-                  AlwaysStoppedAnimation<Color>(Colors.deepPurpleAccent),
-            ),
-          );
-        });
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(
+            // color: Colors.blueAccent,
+            strokeWidth: 4,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.deepPurpleAccent),
+          ),
+        );
+      },
+    );
     var url = Uri.parse('https://api.quotable.io/random');
     var response = await http.get(url);
     debugPrint('Response status: ${response.statusCode}');
@@ -46,70 +48,72 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         backgroundColor: Colors.deepPurple.shade100,
         appBar: AppBar(
-          actions: const [
-            Padding(
-              padding: EdgeInsets.only(right: 20),
-              child: Icon(Icons.menu),
-            )
-          ],
+// actions: const [
+//             Padding(
+//               padding: EdgeInsets.only(right: 20),
+//               child: Icon(Icons.menu),
+//             )
+//           ],
           title: Text(
             "Random Quotes",
             style:
-                GoogleFonts.poppins(textStyle: const TextStyle(fontSize: 15)),
+                GoogleFonts.poppins(textStyle: const TextStyle(fontSize: 18)),
           ),
         ),
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: IntrinsicHeight(
-                child: Container(
-                  // decoration: BoxDecoration(border: Border.all(width: 8)),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple.shade200,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 25, left: 10, right: 10, bottom: 10),
-                        child: Text(
-                          quotes,
-                          textAlign: TextAlign.start,
-                          style: GoogleFonts.poppins(
-                            textStyle: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: .4,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20, right: 30),
-                        child: Align(
-                          alignment: Alignment.bottomRight,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: IntrinsicHeight(
+                  child: Container(
+                    // decoration: BoxDecoration(border: Border.all(width: 8)),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple.shade200,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              top: 25, left: 10, right: 10, bottom: 10),
                           child: Text(
-                            "-$author",
+                            quotes,
+                            textAlign: TextAlign.start,
                             style: GoogleFonts.poppins(
                               textStyle: const TextStyle(
-                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: .4,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20, right: 30),
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: Text(
+                              "-$author",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            newMethod(),
-          ],
+              newMethod(),
+            ],
+          ),
         ),
       ),
     );
@@ -125,6 +129,7 @@ class _HomeScreenState extends State<HomeScreen> {
           // disabledColor: CupertinoColors.quaternarySystemFill,
           onPressed: () {
             // _isLoading = false;
+
             getQuotes();
             setState(() {
               Padding(
